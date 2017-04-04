48c75dd6af4209b8c3c1055f3d60835877feabd6
Keegan Provo <keeganprovo@gmail.com>
2017-04-03 22:02:40 -0600

    Create semi-broken login system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,35 @@
 class User < ApplicationRecord
+  attr_accessor :remember_session_token
+  before_save :downcase_email
+  before_create :remember
   has_secure_password
+
+  def User.digest(string)
+    cost = BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_session_token = User.new_token
+    if self.remember_token
+      update_attribute(:remember_token, User.digest(remember_session_token))
+    else
+      write_attribute(:remember_token, User.digest(remember_session_token))
+    end
+  end
+
+  def authenticated?(token)
+    return false if token.nil?
+    BCrypt::Password.new(:remember_token).is_password?(token)    
+  end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401203750) do
+ActiveRecord::Schema.define(version: 20170403045815) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170401203750) do
     t.boolean  "member"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_token"
   end
 
 end
